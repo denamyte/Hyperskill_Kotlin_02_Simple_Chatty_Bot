7db0b5205b46c883105ae602474c0b7e80c35b3f
denys <denamyte@gmail.com>
2023-03-21 15:47:54 +0200

    2023-03-21: Stage 4/5: finished

--- Simple Chatty Bot (Kotlin)/task/src/bot/SimpleBot.kt
+++ Simple Chatty Bot (Kotlin)/task/src/bot/SimpleBot.kt
@@ -1,24 +1,45 @@
 package bot
 
 import java.util.Scanner
-import kotlin.collections.List
 
 
 fun main() {
     val scanner = Scanner(System.`in`)
 
-    println("Hello! My name is Aid.")
-    println("I was created in 2020.")
+    introduce()
+    greet(scanner)
+    guessAge(scanner)
+    countNumbers(scanner)
+}
+
+private fun countNumbers(scanner: Scanner) {
+    println("Now I will prove to you that I can count to any number you want.")
+    (0..scanner.nextInt()).forEach{ println("$it!") }
+    println("Completed, have a nice day!")
+}
+
+private fun guessAge(scanner: Scanner) {
+    println("Let me guess your age.")
+    println("Enter remainders of dividing your age by 3, 5 and 7.")
+
+    val rem3 = scanner.nextInt()
+    val rem5 = scanner.nextInt()
+    val rem7 = scanner.nextInt()
+    val age = (rem3 * 70 + rem5 * 21 + rem7 * 15) % 105
+
+    println("Your age is " + age + "; that's a good time to start programming!")
+}
+
+private fun greet(scanner: Scanner) {
     println("Please, remind me your name.")
 
     val name = scanner.nextLine()
 
-    println("What a great name you have, $name!")
-    println("Let me guess your age.")
-    println("Enter remainders of dividing your age by 3, 5 and 7.")
-
-    val rems = List(3) { scanner.nextInt() }
-    val age = (rems[0] * 70 + rems[1] * 21 + rems[2] * 15) % 105
-
-    println("Your age is $age; that's a good time to start programming!")
+    println("What a great name you have, " + name + "!")
 }
+
+private fun introduce() {
+    println("Hello! My name is Aid.")
+    println("I was created in 2020.")
+}
+
