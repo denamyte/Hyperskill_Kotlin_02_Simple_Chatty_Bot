950107b773d236aa9210e4a84442a8f23b0326cd
denys <denamyte@gmail.com>
2023-03-21 21:33:55 +0200

    2023-03-21: Stage 5/5: finished

--- Simple Chatty Bot (Kotlin)/task/src/bot/SimpleBot.kt
+++ Simple Chatty Bot (Kotlin)/task/src/bot/SimpleBot.kt
@@ -3,43 +3,70 @@ package bot
 import java.util.Scanner
 
 
+val scanner = Scanner(System.`in`) // Do not change this line
+val TEST_LIST = listOf(
+    "To repeat a statement multiple times.",
+    "To decompose a program into several small subroutines.",
+    "To determine the execution time of a program.",
+    "To interrupt the execution of a program."
+)
+const val TEST_QUESTION = "Why do we use methods?"
+const val TEST_ANSWER = 2
+const val TRY_AGAIN = "Please, try again."
+
 fun main() {
-    val scanner = Scanner(System.`in`)
-
-    introduce()
-    greet(scanner)
-    guessAge(scanner)
-    countNumbers(scanner)
+    greet("Aid", "2020") // change it as you need
+    remindName()
+    guessAge()
+    count()
+    test()
+    end()
 }
 
-private fun countNumbers(scanner: Scanner) {
-    println("Now I will prove to you that I can count to any number you want.")
-    (0..scanner.nextInt()).forEach{ println("$it!") }
-    println("Completed, have a nice day!")
+fun greet(assistantName: String, birthYear: String) {
+    println("Hello! My name is ${assistantName}.")
+    println("I was created in ${birthYear}.")
+    println("Please, remind me your name.")
 }
 
-private fun guessAge(scanner: Scanner) {
+fun remindName() {
+    val name = scanner.nextLine()
+    println("What a great name you have, ${name}!")
+}
+
+fun guessAge() {
     println("Let me guess your age.")
     println("Enter remainders of dividing your age by 3, 5 and 7.")
-
     val rem3 = scanner.nextInt()
     val rem5 = scanner.nextInt()
     val rem7 = scanner.nextInt()
     val age = (rem3 * 70 + rem5 * 21 + rem7 * 15) % 105
-
-    println("Your age is " + age + "; that's a good time to start programming!")
+    println("Your age is ${age}; that's a good time to start programming!")
 }
 
-private fun greet(scanner: Scanner) {
-    println("Please, remind me your name.")
-
-    val name = scanner.nextLine()
-
-    println("What a great name you have, " + name + "!")
+fun count() {
+    println("Now I will prove to you that I can count to any number you want.")
+    val num = scanner.nextInt()
+    for (i in 0..num) {
+        print(i)
+        println("!")
+    }
 }
 
-private fun introduce() {
-    println("Hello! My name is Aid.")
-    println("I was created in 2020.")
+fun test() {
+    println("Let's test your programming knowledge.")
+    println(TEST_QUESTION)
+    TEST_LIST.forEachIndexed { index, s -> println("${index + 1}. $s") }
+    var answer: Int
+    while(true) {
+        answer = readln().toInt()
+        if (answer == TEST_ANSWER) {
+            break
+        }
+        println(TRY_AGAIN)
+    }
 }
 
+fun end() {
+    println("Congratulations, have a nice day!") // Do not change this text
+}
