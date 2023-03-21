bdbb628b123201444265916ceb9b2bc088fc427f
denys <denamyte@gmail.com>
2023-03-21 10:20:13 +0200

    2023-03-21: Stage 3/5: finished

--- Simple Chatty Bot (Kotlin)/task/src/bot/SimpleBot.kt
+++ Simple Chatty Bot (Kotlin)/task/src/bot/SimpleBot.kt
@@ -1,6 +1,7 @@
 package bot
 
 import java.util.Scanner
+import kotlin.collections.List
 
 
 fun main() {
@@ -12,11 +13,12 @@ fun main() {
 
     val name = scanner.nextLine()
 
-    println("What a great name you have, " + name + "!")
+    println("What a great name you have, $name!")
     println("Let me guess your age.")
     println("Enter remainders of dividing your age by 3, 5 and 7.")
 
-    // reading all remainders
+    val rems = List(3) { scanner.nextInt() }
+    val age = (rems[0] * 70 + rems[1] * 21 + rems[2] * 15) % 105
 
-    println("Your age is {yourAge}; that's a good time to start programming!")
+    println("Your age is $age; that's a good time to start programming!")
 }
